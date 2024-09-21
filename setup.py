672fbd52ff12521f0dcd1fc970b774be5b843d6d
jpbreuer <jeanpaul.breuer@gmail.com>
2024-09-21 19:03:41 +0900

    license change

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycontbin',
-    version='0.2.2',    
+    version='0.2.3',    
     description='A Python implementation of the ContBin algorithm',
 	long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/jpbreuer/contbin_python',
     author='Jean-Paul Breuer',
     author_email='jeanpaul.breuer@gmail.com',    
-    license='GNU Public License GPL-3.0',
+    license='GNU General Public License v3 (GPLv3)',
     packages=['pycontbin'],
     install_requires=['numpy',
 					  'astropy',
@@ -18,7 +18,7 @@ setup(
 
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Intended Audience :: Science/Research',
         'Operating System :: POSIX :: Linux',        
     ],
